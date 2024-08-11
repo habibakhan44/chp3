89413904dfa376a2a71b0c488e57e44d6b32dab4
habibakhan44 <152440245+habibakhan44@users.noreply.github.com>
2024-08-12 00:49:43 +0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
- 
+"# JavaScript-tasks" 
+"# javascript-task" 
---
+++ script.js
@@ -0,0 +1,29 @@
+              //    Question 1:     
+var age = 15;
+alert("I am " + age + " years old");
+
+           //    Question 2:     
+var timespersite = 14;
+alert("You have visited this site" +  timespersite + "times");
+
+
+
+
+            //  Question  3:
+var birthyear = 2005;
+document.write("My birth year is "+" " + birthyear + "<br/>");
+document.write("Data type of my declared variable is number");
+
+
+         //    Question 4:     
+var visitorName = "John Doe";
+var productTitle = "T-shirt(s)";
+var quantity = 5;
+
+document.write("<b>" + visitorName + "</b> ordered <b>" + quantity + "</b> <b>" + productTitle + "</b> on XYZ Clothing store.");
+
+
+
+
+
+
